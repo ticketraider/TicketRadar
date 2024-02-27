769c386af59ea90953d10a5f140b3321e324ab28
LSW990918 <leesywon99@gmail.com>
2024-02-27 17:25:55 +0900

    #2 Feat : BaseEntity UpdateIimestamp 추가

--- src/main/kotlin/com/codersgate/ticketraider/global/common/BaseEntity.kt
+++ src/main/kotlin/com/codersgate/ticketraider/global/common/BaseEntity.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
@@ -14,10 +15,10 @@ abstract class BaseEntity {
     @Column(nullable = false, updatable = false)
     val createdAt: LocalDateTime? = null
 
-    @CreationTimestamp
+    @UpdateTimestamp
     @Column(nullable = false)
     val updatedAt: LocalDateTime? = null
 
     @Column(nullable = false)
-    val isDeleted: Boolean = false
+    var isDeleted: Boolean = false
 }
