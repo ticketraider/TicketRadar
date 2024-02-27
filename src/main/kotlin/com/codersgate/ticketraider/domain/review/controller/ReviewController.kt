4a2fe0e78ffbe0d8cdbcccce56cb753cf3ee707f
codingjang <jjangga813@naver.com>
2024-02-27 19:55:36 +0900

    #4 Feat : Review Validation 추가

--- src/main/kotlin/com/codersgate/ticketraider/domain/review/controller/ReviewController.kt
+++ src/main/kotlin/com/codersgate/ticketraider/domain/review/controller/ReviewController.kt
@@ -6,12 +6,14 @@ import com.codersgate.ticketraider.domain.review.dto.UpdateReviewRequest
 import com.codersgate.ticketraider.domain.review.repository.ReviewRepository
 import com.codersgate.ticketraider.domain.review.service.ReviewService
 import io.swagger.v3.oas.annotations.Operation
+import jakarta.validation.Valid
 import org.apache.catalina.User
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.parameters.P
 import org.springframework.web.bind.annotation.*
 
@@ -26,7 +28,7 @@ class ReviewController(
     @Operation(summary = "리뷰 생성")
     @PostMapping()
     fun createReview(
-        @RequestBody request: CreateReviewRequest
+        @Valid @RequestBody request: CreateReviewRequest
     ) : ResponseEntity<Unit>
     {
         reviewService.createReview(request)
@@ -85,7 +87,7 @@ class ReviewController(
     @PutMapping("/{reviewId}")
     fun updateReview(
         @PathVariable reviewId : Long,
-        @RequestBody request : UpdateReviewRequest
+        @Valid @RequestBody request : UpdateReviewRequest
     ) : ResponseEntity<Unit>{
         reviewService.updateReview(reviewId, request)
         return ResponseEntity.status(HttpStatus.OK).build()
