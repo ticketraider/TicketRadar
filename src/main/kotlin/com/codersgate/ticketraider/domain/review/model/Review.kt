67914f9b147f0ee6465677df6a31cdf00e617b71
codingjang <jjangga813@naver.com>
2024-03-06 17:21:45 +0900

    #4 Feature : fetch 타입 수정

--- src/main/kotlin/com/codersgate/ticketraider/domain/review/model/Review.kt
+++ src/main/kotlin/com/codersgate/ticketraider/domain/review/model/Review.kt
@@ -3,14 +3,7 @@ package com.codersgate.ticketraider.domain.review.model
 import com.codersgate.ticketraider.domain.event.model.Event
 import com.codersgate.ticketraider.domain.member.entity.Member
 import com.codersgate.ticketraider.global.common.BaseEntity
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
 
@@ -29,11 +22,11 @@ class Review (
     @Column(name = "rating")
     var rating : Int,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     val member: Member,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
     val event : Event,
 
