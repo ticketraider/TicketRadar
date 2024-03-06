b172e773427ff1241e1200d583623e02e45086aa
LSW990918 <leesywon99@gmail.com>
2024-03-06 12:32:09 +0900

    #49 Test : 테스트코드 어노테이션 수정

--- src/test/kotlin/com/codersgate/ticketraider/domain/member/repository/MemberRepositoryTest.kt
+++ src/test/kotlin/com/codersgate/ticketraider/domain/member/repository/MemberRepositoryTest.kt
@@ -5,9 +5,7 @@ import com.codersgate.ticketraider.domain.member.entity.MemberRole
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
