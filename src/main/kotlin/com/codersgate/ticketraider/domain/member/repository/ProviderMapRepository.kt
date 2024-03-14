c94036117c1a8b51277460a29dc688a06482d8c0
hongdul <kkw0426@naver.com>
2024-03-14 17:14:07 +0900

    Feat: ProviderMapRepository에 삭제된 ProviderMap 찾는 native query 생성 #79 - 입력받은 memberId로 해당 ProviderMap 찾아서 반환 - ProviderMap 엔티티에 SQLRestriction 때문에 native query 사용해 조회

--- src/main/kotlin/com/codersgate/ticketraider/domain/member/repository/ProviderMapRepository.kt
+++ src/main/kotlin/com/codersgate/ticketraider/domain/member/repository/ProviderMapRepository.kt
@@ -1,8 +1,14 @@
 package com.codersgate.ticketraider.domain.member.repository
 
 import com.codersgate.ticketraider.domain.member.entity.ProviderMap
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface ProviderMapRepository: CrudRepository<ProviderMap, Long>{
+interface ProviderMapRepository: JpaRepository<ProviderMap, Long>{
     fun findByMemberId(memberId: Long): ProviderMap
+    fun deleteByMemberId(memberId: Long) : Any
+    @Query(value = "select * from provider_map where member_id = :memberId limit 1", nativeQuery = true)
+    fun restoreProviderMapByMemberId(@Param("memberId") memberId: Long): ProviderMap?
+
 }
