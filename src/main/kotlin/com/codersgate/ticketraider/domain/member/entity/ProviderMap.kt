313e2dcea53284a4515beca9ca9e5ba9527cfa60
hongdul <kkw0426@naver.com>
2024-03-14 17:16:29 +0900

    Feat: ProviderMap 엔티티에 soft delete 적용 #79 - SQLDelete -> 삭제시 is_deleted = true - SQLRestriction -> is_deleted가 false 인 엔티티만 조회 - is_deleted = false 컬럼 추가

--- src/main/kotlin/com/codersgate/ticketraider/domain/member/entity/ProviderMap.kt
+++ src/main/kotlin/com/codersgate/ticketraider/domain/member/entity/ProviderMap.kt
@@ -4,17 +4,20 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
 
+@SQLDelete(sql = "UPDATE provider_map SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Entity
 class ProviderMap(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
 
     var isCommonMember: Boolean = false,
     var isKakaoMember: Boolean = false,
     var isNaverMember: Boolean = false,
     var isGoogleMember: Boolean = false,
-    val memberId: Long
+    val memberId: Long,
+    var isDeleted: Boolean = false,
 ) {
 
     fun registerSocialInfo(socialInfo: Provider) {
