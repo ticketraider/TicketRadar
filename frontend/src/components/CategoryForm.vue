b9c698b777bdde689a3dd8c794b4b5cf8c99f464
codingjang <jjangga813@naver.com>
2024-03-25 21:20:15 +0900

    #76 Feat : 카테고리 생성/수정 구현 서버로 요청 전송시 headers: {       Authorization: `Bearer ${token}` // JWT 토큰을 포함한 Authorization 헤더 설정     } 과정 필요

--- frontend/src/components/CategoryForm.vue
+++ frontend/src/components/CategoryForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form style="color: white; width: 500px; height: 400px; margin: 100px auto auto auto" @submit.prevent="createCategory">
+ <form style="color: white; width: 500px; height: 400px; margin: 100px auto auto auto" > <!-- @submit.prevent="createCategory"-->
     <div style="background-color: #392365; text-align: center; border-radius: 5px; margin-bottom: 20px">
       <h2>카테고리</h2>
     </div>
@@ -20,14 +20,38 @@
               <input v-model="newCategory.title" type="text" class="form-control" id="categoryTitle">
             </div>
             <div style="text-align: right;">
-              <button type="submit" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력</button>
+              <button @click="createCategory" type="submit" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력</button>
             </div>
           </div>
         </div>
       </div>
 
       <!-- 카테고리 수정 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            카테고리 수정
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
 
+            <div class="mb-3">
+              <label for="categoryId" class="form-label">카테고리 ID</label>
+              <input v-model="newCategory.categoryId"  type="number" class="form-control" id="exampleInputEmail1"
+                     aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="categoryTitle" class="form-label">타이틀</label>
+              <input v-model="newCategory.title" type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div style="text-align: right;">
+              <button @click="updateCategory" type="submit" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;"> 입력 </button>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <!-- 카테고리 삭제 -->
 
@@ -39,20 +63,28 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
+// 로컬 스토리지에서 JWT 토큰 가져오기
+const token = localStorage.getItem('token');
+
 // 새 카테고리를 나타내는 데이터
 const newCategory = ref({
+  categoryId : '',
   title: ''
 });
 
 // 카테고리를 생성하는 함수
 const createCategory = () => {
-  // 서버에 보낼 데이터
-  const requestData = {
-    title: newCategory.value.title
-  };
 
   // 서버로 POST 요청 보내기
-  axios.post('http://localhost:8080/categories', requestData)
+  axios.post('http://localhost:8080/categories',
+      {
+        title: newCategory.value.title
+      },
+      {
+    headers: {
+      Authorization: `Bearer ${token}` // JWT 토큰을 포함한 Authorization 헤더 설정
+    }
+  })
       .then(response => {
         console.log('카테고리 생성 성공:', response.data);
         // 성공적으로 생성되었을 때의 처리 (예: 페이지 리로드)
@@ -60,6 +92,38 @@ const createCategory = () => {
       })
       .catch(error => {
         console.error('카테고리 생성 오류:', error);
+        // 실패 시에도 현재 헤더에 포함된 토큰 확인
+        console.log('현재 헤더:', error.config.headers);
+        // 오류 발생 시 사용자에게 알림
+        alert('카테고리 생성에 실패했습니다.');
+      });
+};
+
+const updateCategory = () => {
+  const categoryId = parseInt(newCategory.value.categoryId)
+
+  console.log("categoryId : " , categoryId)
+  console.log("title : ", newCategory.value.title)
+
+  // 서버로 POST 요청 보내기
+  axios.put(`http://localhost:8080/categories/${categoryId}`,
+      {
+        title: newCategory.value.title
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}` // JWT 토큰을 포함한 Authorization 헤더 설정
+        }
+      })
+      .then(response => {
+        console.log('카테고리 생성 성공:', response.data);
+        // 성공적으로 생성되었을 때의 처리 (예: 페이지 리로드)
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('카테고리 생성 오류:', error);
+        // 실패 시에도 현재 헤더에 포함된 토큰 확인
+        console.log('현재 헤더:', error.config.headers);
         // 오류 발생 시 사용자에게 알림
         alert('카테고리 생성에 실패했습니다.');
       });
