2272c308828009f566ae4a894138f7c984145826
codingjang <jjangga813@naver.com>
2024-03-26 16:35:57 +0900

    #76 Feat : 이벤트 삭제 성공. 수정 구현중

--- frontend/src/components/BackOffice/EventDeleteForm.vue
+++ frontend/src/components/BackOffice/EventDeleteForm.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="accordion-item">
     <h2 class="accordion-header">
@@ -12,20 +8,34 @@
     </h2>
     <div id="deleteEvent" class="accordion-collapse collapse" data-bs-parent="#event">
       <div class="accordion-body">
-
         <div class="mb-3">
-          <input type="password" placeholder="이벤트 ID" class="form-control" id="exampleInputPassword1">
+          <input type="number" v-model="eventId" placeholder="이벤트 ID" class="form-control" id="exampleInputPassword1">
         </div>
         <div style="text-align: right;">
-          <button type="submit" class="btn btn-primary"
+          <button @click="deleteEvent" class="btn btn-primary"
                   style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력
           </button>
         </div>
-
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const eventId = ref(null);
+
+const deleteEvent = async () => {
+  try {
+    await axios.delete(`http://localhost:8080/events/${eventId.value}`);
+    console.log('이벤트가 성공적으로 삭제되었습니다!');
+  } catch (error) {
+    console.error('이벤트 삭제 중 오류 발생:', error);
+  }
+};
+</script>
 
 <style scoped>
 
