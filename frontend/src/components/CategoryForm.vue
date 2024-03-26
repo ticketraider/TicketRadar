30a6576415607c243a1744d7871c679a734f7dac
codingjang <jjangga813@naver.com>
2024-03-26 12:05:54 +0900

    #76 Feat : 카테고리 삭제 구현 버튼 타입 submit 으로 설정시 alert 안떠서 실패 여부 확인 어려움. 테스트 후 submit 으로 변경

--- frontend/src/components/CategoryForm.vue
+++ frontend/src/components/CategoryForm.vue
@@ -17,10 +17,10 @@
           <div class="accordion-body">
             <div class="mb-3">
               <label for="categoryTitle" class="form-label">타이틀</label>
-              <input v-model="newCategory.title" type="text" class="form-control" id="categoryTitle">
+              <input v-model="newCategory.createTitle" type="text" class="form-control" id="categoryTitle">
             </div>
             <div style="text-align: right;">
-              <button @click="createCategory" type="submit" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력</button>
+              <button @click="createCategory" type="button" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력</button>
             </div>
           </div>
         </div>
@@ -39,22 +39,44 @@
 
             <div class="mb-3">
               <label for="categoryId" class="form-label">카테고리 ID</label>
-              <input v-model="newCategory.categoryId"  type="number" class="form-control" id="exampleInputEmail1"
+              <input v-model="newCategory.updateId"  type="number" class="form-control" id="exampleInputEmail1"
                      aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
               <label for="categoryTitle" class="form-label">타이틀</label>
-              <input v-model="newCategory.title" type="text" class="form-control" id="exampleInputPassword1">
+              <input v-model="newCategory.updateTitle" type="text" class="form-control" id="exampleInputPassword1">
             </div>
             <div style="text-align: right;">
-              <button @click="updateCategory" type="submit" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;"> 입력 </button>
+              <button @click="updateCategory" type="button" class="btn btn-primary" style="background-color: #392365; border-color: #392365; margin-left: 15px;"> 입력 </button>
             </div>
           </div>
         </div>
       </div>
 
       <!-- 카테고리 삭제 -->
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            카테고리 삭제
+          </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
 
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">카테고리 ID</label>
+              <input v-model="newCategory.deleteId" type="number" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div style="text-align: right;">
+              <button @click="deleteCategory" type="button" class="btn btn-primary"
+                      style="background-color: #392365; border-color: #392365; margin-left: 15px;">입력
+              </button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   </form>
 </template>
@@ -68,8 +90,10 @@ const token = localStorage.getItem('token');
 
 // 새 카테고리를 나타내는 데이터
 const newCategory = ref({
-  categoryId : '',
-  title: ''
+  createTitle : '',
+  updateId : '',
+  updateTitle : '',
+  deleteId : '',
 });
 
 // 카테고리를 생성하는 함수
@@ -78,7 +102,7 @@ const createCategory = () => {
   // 서버로 POST 요청 보내기
   axios.post('http://localhost:8080/categories',
       {
-        title: newCategory.value.title
+        title: newCategory.value.createTitle
       },
       {
     headers: {
@@ -100,15 +124,12 @@ const createCategory = () => {
 };
 
 const updateCategory = () => {
-  const categoryId = parseInt(newCategory.value.categoryId)
-
-  console.log("categoryId : " , categoryId)
-  console.log("title : ", newCategory.value.title)
+  const categoryId = parseInt(newCategory.value.updateId)
 
   // 서버로 POST 요청 보내기
   axios.put(`http://localhost:8080/categories/${categoryId}`,
       {
-        title: newCategory.value.title
+        title: newCategory.value.updateTitle
       },
       {
         headers: {
@@ -116,8 +137,8 @@ const updateCategory = () => {
         }
       })
       .then(response => {
-        console.log('카테고리 생성 성공:', response.data);
         // 성공적으로 생성되었을 때의 처리 (예: 페이지 리로드)
+        console.log('카테고리 생성 성공:', response.data);
         window.location.reload();
       })
       .catch(error => {
@@ -128,6 +149,31 @@ const updateCategory = () => {
         alert('카테고리 생성에 실패했습니다.');
       });
 };
+
+const deleteCategory = () => {
+  const categoryId = parseInt(newCategory.value.deleteId)
+
+  // 서버로 POST 요청 보내기
+  axios.delete(`http://localhost:8080/categories/${categoryId}`, {
+        headers: {
+          Authorization: `Bearer ${token}` // JWT 토큰을 포함한 Authorization 헤더 설정
+        }
+      })
+      .then(response => {
+        console.log('카테고리 삭제 성공:', response.data);
+        // 성공적으로 생성되었을 때의 처리 (예: 페이지 리로드)
+        alert('카테고리 삭제에 성공했습니다.');
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('카테고리 삭제 오류:', error);
+        // 실패 시에도 현재 헤더에 포함된 토큰 확인
+        console.log('현재 헤더:', error.config.headers);
+        // 오류 발생 시 사용자에게 알림
+        alert('카테고리 삭제에 실패했습니다.');
+      });
+};
+
 </script>
 
 <style scoped>
